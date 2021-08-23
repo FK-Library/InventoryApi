b7400f2b469309ab6ad557bc5745d76c089ab342
FK-Library <fatemeh.khodadadi@outlook.com>
2021-08-23 21:11:55 +0100

    base Api - built

---
+++ ShopApi/ShopApi/Controllers/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using InventoryApi.Models;
+using InventoryApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InventoryApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InventoryController : ControllerBase
+    {
+        private readonly ILogger<InventoryController> _logger;
+        private readonly InventoryService _inventoryService;
+
+        public InventoryController(ILogger<InventoryController> logger)
+        {
+            _logger = logger;
+            _inventoryService = new InventoryService();
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _inventoryService.GetProducts();
+        }
+
+    }
+}
---
+++ ShopApi/ShopApi/Services/InventoryService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using InventoryApi.Models;
+
+namespace InventoryApi.Services
+{
+    public class InventoryService
+    {
+
+        private static string GetJsonData()
+        {
+            string path = Directory.GetCurrentDirectory();
+            var dataStore = new FileStream(System.IO.Path.Join(path, "data.json"), FileMode.Open);
+            using var reader = new StreamReader(dataStore);
+            return  reader.ReadToEnd();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            var data= GetJsonData();
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<Product>>(data);
+        }
+
+    }
+}
